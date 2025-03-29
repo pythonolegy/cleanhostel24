33a91deb0dcfac0cad04cb46d93f3c2b60963723
pythonolegy <prostodonald@gmail.com>
2025-03-29 12:33:14 +0300

    fixed getenv

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from http.client import HTTPException
 
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import models
+from .database import SessionLocal, engine
+
+
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "CleanHost24"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+@app.post("/rooms/")
+def create_room(name: str, db: Session = Depends(get_db)):
+    db_room = db.query(models.Room).filter(models.Room.name == name).first()
+    if db_room:
+        raise HTTPException(status_code=400, detail="Room already exists")
+
+    new_room = models.Room(name=name)
+    db.add(new_room)
+    db.commit()
+    db.refresh(new_room)
+    return new_room
+
+@app.get("/rooms/")
+def get_rooms(db: Session = Depends(get_db)):
+    rooms = db.query(models.Room).all()
+    return rooms
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,13 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@localhost/cleanhostel"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
